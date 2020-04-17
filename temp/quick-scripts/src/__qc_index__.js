c3afa4b4111f79b7a5475d50061e2540a139667e
riyanpro <764733342@qq.com>
2020-04-17 15:56:06 +0800

    ignore cocos files

--- temp/quick-scripts/src/__qc_index__.js
+++
@@ -1,6 +0,0 @@
-
-require('./assets/JS/js_cardRegion');
-require('./assets/JS/js_dataRegion');
-require('./assets/JS/js_home');
-require('./assets/JS/js_httpRequest');
-require('./assets/migration/use_reversed_rotateBy');

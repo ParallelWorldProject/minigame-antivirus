cc2f6fca20ce4e7ffa2e905fc90f4983be185c38
Chentepic <46065549+Chentepic@users.noreply.github.com>
2020-04-23 13:34:20 +0800

    无修改

--- assets/JS/cardRegion/js_cardTemplate.js
+++ assets/JS/cardRegion/js_cardTemplate.js
@@ -40,17 +40,7 @@ cc.Class({
         //this.loadImg( this.image.getComponent(cc.Sprite) ,"resources/img/cardimg_2" )
     },     
        
-    moveOff : function()
-    {
-        this.node.active = false;
-    },
-
-    moveUp : function()
-    {
-        this.node.active = true;
-    },
-  
-
+    
     //远程加载  
     loadImg: function(container,url){
         cc.loader.load(url, function (err, texture) {
